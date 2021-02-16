0798db8d3384748e3d2ddd1dc5e5518e7b184787
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-02-16 20:02:41 +0100

    Add state and handle values functions in UserForm

--- src/components/features/UserForm/UserForm.js
+++ src/components/features/UserForm/UserForm.js
@@ -5,6 +5,25 @@ import styles from './UserForm.module.scss';
 import { Link } from 'react-router-dom';
 
 class UserForm extends React.Component {
+  state = {
+    email: '',
+    name: '',
+  }
+
+  handleEmailChange(newValue) {
+    this.setState({
+      ...this.state,
+      email: newValue,
+    });
+  }
+
+  handleNameChange(newValue) {
+    this.setState({
+      ...this.state,
+      name: newValue,
+    });
+  }
+
   render() {
     return (
       <section className={clsx('container-xl', styles.userForm)}>
@@ -12,11 +31,23 @@ class UserForm extends React.Component {
         <form>
           <div className='mb-2'>
             <label htmlFor='email' className='form-label'>Email</label>
-            <input type="email" className='form-control' id="email"/>
+            <input 
+              type="email" 
+              className='form-control' 
+              id="email"
+              value={this.state.email}
+              onChange={e => this.handleEmailChange(e.target.value)}
+            />
           </div>
           <div className='mb-2'>
             <label htmlFor='name' className='form-label'>Name</label>
-            <input type='text' className='form-control' id='name'/>
+            <input 
+              type='text' 
+              className='form-control' 
+              id='name'
+              value={this.state.name}
+              onChange={e => this.handleNameChange(e.currentTarget.value)}
+            />
           </div>
         </form>
         <div className='row justify-content-end'>
