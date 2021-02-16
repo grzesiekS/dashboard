efd8261ff0c63dbf977d95f17607ce334e5e6f39
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-02-16 17:32:20 +0100

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './styles/global.scss';
+
+import MainLayout from './components/layout/MainLayout/MainLayout';
+
+const App = () => (
+  <MainLayout>
+    
+  </MainLayout>
+);
 
 export default App;
