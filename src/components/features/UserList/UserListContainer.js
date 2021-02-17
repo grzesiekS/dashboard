1225919f4731f8b7f61e39bee322398c3db3f286
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-02-17 20:38:28 +0100

    Convert App to be able to handle delete user in local storage

--- src/components/features/UserList/UserListContainer.js
+++ src/components/features/UserList/UserListContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import UserList from './UserList';
 
-import { getUsersData, fetchUsers, restoreDefaultUserData, deleteUserAPI } from '../../../redux/usersRedux';
+import { getUsersData, fetchUsers, restoreDefaultUserData, deleteUserAPI, deleteSuccess } from '../../../redux/usersRedux';
 
 const mapStateToProps = state => ({
   userList: getUsersData(state),
@@ -11,6 +11,8 @@ const mapDispatchToProps = dispatch => ({
   fetchUsers: () => dispatch(fetchUsers()),
   restoreDefaultUserData: () => dispatch(restoreDefaultUserData()),
   deleteUser: id => dispatch(deleteUserAPI(id)),
+  /* Only to handle local data because of the test API */
+  deleteSuccess: id => dispatch(deleteSuccess(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserList);
